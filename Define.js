dcb2f7755c1e7fc102f8acf76f1c8bf588e16303
Akshay Kulkarni <akshayjk10@gmail.com>
2015-04-19 00:12:36 +0530

    Favicon and Define file

---
+++ Define.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Akshay on 18-04-2015.
+ */
+
+var mongoUrl = "mongodb://127.0.0.1:27017/WebSite";
+var StudentCollection = "students";
+var OrdersCollection = "orders";
+
+exports.mongoUrl = mongoUrl;
+exports.studentsCollection = StudentCollection;
+exports.ordersCollection = OrdersCollection;
